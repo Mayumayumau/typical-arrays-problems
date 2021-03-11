e6299c0d32be33583cf5f904fb7d2f3429a52792
Maya <mayasidorova@yahoo.com>
2021-03-11 13:34:20 +0800

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array) return 0;
+  let min = array[0] || 0;
+  array.forEach(element => {
+    if (min > element) min = element;
+  })
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array) return 0;
+  let max = 0;
+  array.forEach(element => {
+    if (max < element) max = element;
+  })
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array) return 0;
+  let sum = 0;
+  array.forEach(element => {sum += element})
+  return array.length ? sum / array.length : 0;
 }
